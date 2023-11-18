03b38be54eac4471da9af37f3475d3715c12ecac
brunosarti83 <brunosarti.bs@gmail.com>
2023-11-18 12:50:59 -0300

    fix(telegram url)

--- telegram.py
+++ telegram.py
@@ -1,11 +1,12 @@
 
 #SI SE ROMPE, TUTORIAL: https://www.youtube.com/watch?v=ps1yeWwd6iA
-from config_futures import CHAT_ID
+from config_futures import CHAT_ID, TELE_BOT
 
 def send_telegram(message):
     import requests
     chatId = CHAT_ID
-    url = 'https://api.telegram.org/bot1700277460:AAG4YdeSJZ0iZEYmambFKZN5pZTEVMb-YQ8/sendMessage?chat_id='+chatId+'&text="{}"'.format(message)
+    teleBot = TELE_BOT
+    url = 'https://api.telegram.org/'+teleBot+'/sendMessage?chat_id='+chatId+'&text="{}"'.format(message)
     requests.get(url)
     return
 
